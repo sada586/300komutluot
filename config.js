bf413939d8ba2ae0dba7a6792f6401b52e90525c
sada586 <67920767+sada586@users.noreply.github.com>
2021-06-23 12:38:01 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    "ownerID": "İD", //kendi IDınızı yazınız
+    "admins": ["İD"],
+    "support": ["İD"],
+    "token": "TOKEN", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "", //botunuzun secretini yazınız
+      "callbackURL": `https://vortex-altyaps.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "super-secret-session-thing", //kalsın
+      "domain": "https://vortex-altyaps.glitch.me", //site URLnizi yazınız!
+          "port": process.env.PORT
+    }
+  };
+  
+  module.exports = config;
+
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+
+//GİZLYİE ALMASANIZ SUNUCUNUZ PATLIYABİLİR
